7e2d877b2c8ed0e95d169e24c69647e7506915bf
Florian Patzke <florian.patzke@t-online.de>
2024-01-23 22:52:45 +0100

    added hoc with runtime check to give errors if DEF component is not unique and if DEF and USE are using different component types.

--- src/withDefUse.tsx
+++ src/withDefUse.tsx
@@ -3,13 +3,16 @@ import { DefStore, UseStore } from './Stores';
 
 
 const useSharedState: (store: typeof DefStore, topic?: string | symbol) => {} = (store, topic) => {
-  
+
   const [state, setState] = useState(topic ? store(topic).getState() : {})
-  
+
   useEffect(() => topic ? store(topic).subscribe((newState) => setState(newState)) : () => { }, [topic])
 
   return state;
 };
+
+
+
 
 
 interface BaseType {
@@ -29,6 +32,47 @@ interface TypeWithUse<T> extends BaseType {
 
 type CombinedType<T> = (TypeWithUse<T> | TypeWithDef<T>) & T;
 
+const DEFInstanceMap = new Map<string | symbol, React.ComponentType<any>>();
+
+const withUniqueDef = <P extends {}>(Component: React.ComponentType<P>) => (p: CombinedType<P>) => {
+  const { DEF } = p;
+
+  if (DEF) {
+    if (DEFInstanceMap.has(DEF)) {
+      console.error(`Identifier "${DEF.toString()}" has to be unique.`);
+      return null;
+    } else {
+      DEFInstanceMap.set(DEF, Component);
+    }
+  }
+
+  return <Component {...p} />;
+};
+
+const USEInstanceMap = new Map<string | symbol, React.ComponentType<any>>();
+
+const withConsistentDefUse = <P extends {}>(Component: React.ComponentType<P>) => (p: CombinedType<P>) => {
+  const { DEF, USE } = p;
+
+  if (DEF && USEInstanceMap.has(DEF) && USEInstanceMap.get(DEF) !== Component) {
+    console.error(`Component with USE="${DEF.toString()}" must match the type of the DEF component.`);
+    return null;
+  }
+
+  if (USE) {
+    if (!DEFInstanceMap.has(USE)) {
+      console.warn(`No instance with DEF="${USE.toString()}" found.`);
+    } else if (DEFInstanceMap.get(USE) !== Component) {
+      console.error(`Components using the same identifier "${USE.toString()}" have to be of the same type.`);
+      return null;
+    }
+    USEInstanceMap.set(USE, Component); // Register the USE component type
+  }
+
+  return <Component {...p} />;
+};
+
+
 const withDefUse = <P extends {}>(Component: React.ComponentType<P>) => (p: CombinedType<P>) => {
   const { DEF, USE, ...props } = p;
 
@@ -40,4 +84,4 @@ const withDefUse = <P extends {}>(Component: React.ComponentType<P>) => (p: Comb
   return <Component {...{ ...props, ...useState, ...routeState } as unknown as P} />
 }
 
-export { withDefUse }
+export { withDefUse, withConsistentDefUse, withUniqueDef }
